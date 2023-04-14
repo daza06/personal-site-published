fe87c54970727913a74077f3c1ae3eb7f5c214d1
Daniel Zavala <link123852@gmail.com>
2023-04-13 22:54:36 -0600

    creating progress bar and scroll to top button

--- app.js
+++ app.js
@@ -17,8 +17,50 @@
 //   navMenu.classList.remove("active");
 // }
 
+// Modal
 $(function () {
   $("#contactButton, #contactFooter").click(function () {
     $("#reserveModal").modal("show");
   });
+});
+
+// Scroll to top button
+const showOnPx = 200;
+const backToTopButton = document.querySelector(".back-to-top");
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+
+backToTopButton.addEventListener("click", goToTop);
+
+// Progress Bar
+const pageProgressBar = document.querySelector(".progress-bar");
+document.addEventListener("scroll", () => {
+  const scrolledPercentage =
+    (scrollContainer().scrollTop /
+      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+    100;
+
+  pageProgressBar.style.width = `${scrolledPercentage}%`;
+
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
 });
